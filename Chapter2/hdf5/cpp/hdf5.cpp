7e27247209f85cd8a97b3939344f4a438b207c54
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-03-21 00:44:12 -0700

    modified:   hdf5.cpp

--- Chapter2/hdf5/cpp/hdf5.cpp
+++ Chapter2/hdf5/cpp/hdf5.cpp
@@ -72,12 +72,22 @@ int main(int argc, char** argv){
 
 
 
-                //We insert 
+                //We insert into each reviews_group a dataset of numerical values of confidence, evaluation, and orientation fields.
+                // For the dataset, we define the DataSpace(the number of elements in the dataset) of size 3 and define a storage type
+                // as a 32-bit integer, as follows:
                 std::vector<size_t> dims = {3};
                 std::vector<int32_t> values(3);
 
 
-                
+                /*
+                After we have created and initialized all objects, the Papers/Reviews dataset in HDF5
+                format is ready. When the file object leaves the scope, its destructor saves everything
+                to the secondary storage.
+
+                Having the file in the HDF5 format, we can consider the HighFive library interface for file
+                reading. As the first step, we again create the HighFive::File object, but with attributes for reading,
+                as follows: 
+                */
                 for (const auto& r: paper.reviews){
                     auto dataset = reviews_group.createDataSet<int32_t>(
                         std::to_string(r.id), HighFive::DataSpace(dims));
@@ -92,9 +102,27 @@ int main(int argc, char** argv){
 
             //read dataset
             {
+              /*
+              After we have created an attribute, we have to put in its value with the write() method.
+              Notice that the HighFive::DataSpace::From function automatically detects the size of the
+              attribute value. The size is the amount of memory required to hold the attribute's value.
+              */
+             //Having the file in the HDF5 format, we can consider the HighFive library interface for file
+             // reading. As the first step, we again create the HighFive::File object, but with attributes 
+             // for reading, as follows:
               HighFive::File file(file_name, HighFive::File::ReadOnly);
+
+
+              //Then, we use the getGroup() method to get the top-level papers_group object, as follows:
               auto papers_group = file.getGroup("papers");
+
+              //We need to get a list of all nested objects in the group because we can access objects only by their names.
+              //We can do this by running the following code: 
               auto papers_names = papers_group.listObjectNames();
+
+              //For each paper object, we read its attributes, and the memory space required for the attribute value.
+              //Also, because each attribute can be multidimensional, we should take care of it and allocate an
+              //appropriate container, as follows:
               for (const auto& pname : papers_names) {
                   auto paper_group = papers_group.getGroup(pname);
                   std::vector<uint32_t> id;
@@ -105,6 +133,10 @@ int main(int argc, char** argv){
                   paper_group.getAttribute("preliminary_decision").read(decision);
                   output << " " << decision << std::endl;
 
+
+
+                  //For reading datasets, we can use the same approach: get the reviews group, then get
+                  //a list of dataset names, and finally, read each dataset in a loop, as follows:
                   auto reviews_group = paper_group.getGroup("reviews");
                   auto reviews_names = reviews_group.listObjectNames();
                   std::vector<int32_t> values(2);
@@ -130,3 +162,14 @@ int main(int argc, char** argv){
 
 return 0;
 }
+
+/*
+Notice that we use the select() method for the dataset, which allows us to read only a part of the 
+dataset. We define this part with ranges given as arguments. There is the read() method in the dataset
+type to read a whole dataset at once.
+
+
+Using these techniques, we can read and transform any HDF5 datasets. This file format allows us to work only with
+part of the required data and not to load the whole file to the memory. Also, because this is a binary format, its 
+reading is more efficient than reading large text files.
+*/
