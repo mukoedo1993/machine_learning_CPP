0476c5fb6d5dbb21681e12e193f886c49b9be6f9
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-04-14 19:06:36 -0700

    modified:   functions_base/DoHierarhicalClustering.cpp

--- Chapter4/dlib/functions_base/DoHierarhicalClustering.cpp
+++ Chapter4/dlib/functions_base/DoHierarhicalClustering.cpp
@@ -22,11 +22,24 @@ using PointCoords = std::pair<Coords, Coords>;
 using Clusters = std::unordered_map<size_t, PointCoords>;
 
 
+/*
+The Dlib lib. implements the agglomerative hierarchical (bottom-up)
+clustering algorithm. The bottom_up_cluster() function implements this
+algorithm. This function takes the matrix of distances between dataset
+objects, the cluster indices container (as the output parameter), and the number 
+of clusters as input parameters. Notice that it returns the container with cluster
+indices in the order of distances provided in the matrix.
+*/
+
 template <typename I>
 void DoHierarhicalClustering (const I& inputs,
                                size_t num_clusters,
                                const std::string& name) {
 // agglomerative clustering algorithm
+/*
+In the following code example, we fill the distance matrix with pairwise Euclidean 
+distance, between each pair of elements in the input dataset:
+*/
 matrix<double> dists(inputs.nr(), inputs.nr());
 for ( long r = 0; r < dists.nr(); ++r){
     for ( long c = 0; c < dists.nc(); ++c){
@@ -34,6 +47,13 @@ for ( long r = 0; r < dists.nr(); ++r){
     }
   }
   std::vector<unsigned long> clusters;
+
+  /*
+  The bottom_up_cluster() function call filled the clusters object with cluster index values,
+  which we can use to visualize the clustering result, as illustrated in the following result:
+  In the preceding screenshot, we can see how the hierarchical clustering algo. implemented in the dlib
+  lib. works on different artificial datasets.
+  */
   bottom_up_cluster(dists, clusters, num_clusters);
   Clusters plot_clusters;
   for (long i = 0; i != inputs.nr(); i++){
