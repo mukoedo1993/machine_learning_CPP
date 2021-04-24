640812b14e779f905b75cb4061a7513bd34a9037
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-04-24 11:17:55 -0700

    Update some files.

---
+++ Chapter5/dlib/isolation_forest.h
@@ -0,0 +1,16 @@
+#ifndef ISOLATION_FOREST_H
+#define ISOLATION_FOREST_H
+
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <memory>
+#include <random>
+#include <unordered_set>
+#include <vector>
+
+namespace iforest {
+
+}
+
+#endif //ISOLATION_FOREST_H
