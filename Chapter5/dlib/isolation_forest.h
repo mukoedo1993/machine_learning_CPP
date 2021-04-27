b2008ff6f3847a99cd5fff9bcd31e3edc8e9d824
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-04-27 13:47:21 -0700

    modified:   dlib/isolation_forest.h

--- Chapter5/dlib/isolation_forest.h
+++ Chapter5/dlib/isolation_forest.h
@@ -8,7 +8,15 @@
 #include <random>
 #include <unordered_set>
 #include <vector>
+/*
+Isolation forest algorithms can be easily implemented in pure C++ because its logic is pretty straightforward.
+Also, there are no implementations of this algorithm in popular libraries. Let's assume that our implementation will
+only be used with two-dimensional data. We are going to detect anomalies in a range of samples where each sample contains
+the same number of features.
 
+Because our dataset is large enough, we can define a wrapper for the actual data container. This allows us to reduce the 
+number of copy operations we perform on the actual data:
+*/
 namespace iforest {
 
     using DataType = double;
@@ -27,6 +35,11 @@ namespace iforest {
 
     template <size_t Cols>
     struct DatasetRange {
+        /*
+        The DatasetRange type holds a reference to the vector of Sample type objects and to the container of indices
+        that point to the samples in the dataset. These indices define the exact dataset objects that this DatasetRange
+        object points to.
+        */
         DatasetRange(std::vector<size_t>&& indices_, /*rvalue reference here*/
                     const Dataset<Cols>* dataset_)
                     : indices(std::move(indices_)), dataset(dataset_) {}
@@ -39,8 +52,11 @@ namespace iforest {
         const Dataset<Cols>* dataset;
     };
 
-
+    // Next, we define the elements of the isolation tree, with the first one being the Node type:
     struct Node {
+        /*
+        This 
+        */
         Node() {}
         Node (const Node&) = delete;
         Node& operator=(const Node&) = delete;
@@ -65,11 +81,27 @@ namespace iforest {
         DataType split_value{0};
         size_t size{0};
         bool is_external{false};
-
+        // This type is a regular tree node structure. The following memebers are specific to the
+        // isolation tree algorithm:
+        /*
+        split_col : This is the index of the feature column where the algorithm caused a split.
+        split_value : This is the value of the feature where the algorithm caused a split.
+        size : This is the number of underlying items for the node.
+        is_external: This is the flag that indicates whether the node is a leaf.
+        */
     };
 
 
-
+    /*
+    Taking the Node type as a basis, we can define the procedure of building an isolation tree.
+    We aggregate this procedure with the auxiliary IsolationTree type. Because the current algorithm
+    is based on random splits, the auxilliary data is the random engine forest.
+    */
+    /*
+    We only need to initialize this object once, and then it will be shared all tree nodes objects.
+    This approach allows us to make the results of the algorithm reproducible in the case of 
+    constant seeding. Furthermore, it makes debugging the randomized algorithm much simpler:
+    */
     template <size_t Cols>
     class IsolationTree {
         public:
@@ -90,6 +122,10 @@ namespace iforest {
             return PathLength(sample, root.get(), 0);
         }
         private:
+        /*
+        Another vital method of the IsolationTree type is the PathLength() method.
+        
+        */
         double PathLength(const Sample<Cols>& sample,
                          const Node* node,
                          double height) {
@@ -106,7 +142,19 @@ namespace iforest {
            }
         }
 
+        /*
+        Next, we'll do the most critical work in the MakeIsolationTree() method, which is used
+        in the ctor. to initialize the root data member:
 
+        Initially, we checked the termination conditions to stop the splitting process. If we meet them,
+        we return a new node marked as an external leaf. Otherwise, we start splitting the passed data range.
+        For splitting, we randomly select the feature column and determine the unique values of the selected feature.
+         Then, we randomly select a value from an interval between the max and min values among the feature values from 
+         all the samples. After we make these random selections, we compare the values of the selected splitting feature to 
+         all the samples from the input data range and put their indices into two lists. One lust is for values higher than 
+         the splitting values, while another list is for values that are lower than them. Then, we return a new tree node initialized
+         with references to the left and right nodes, which are initialized with recursive calls to the MakeIsolationTree() method.
+        */
         std::unique_ptr<Node> MakeIsolationTree(const Data& data,
                                                 size_t height,
                                                 size_t hlim) {
@@ -155,6 +203,22 @@ namespace iforest {
           std::mt19937* rand_engine;
           std::unique_ptr<Node> root;
     };
+
+
+
+
+
+    /*
+    Initially, we checked the termination conditions to stop the splitting process. If we meet them,
+    we return a new node marked as an external leaf. Otherwise, we start splitting the passed data
+    range. For splitting, we randomly select the feature column and determine the unique values of the 
+    selected feature. Then, we randomly select a value from an interval between the max and the min values among
+    the feature values from all the samples. After we make these random selections, we compare the values of selected
+    splitting feature to all the samples from the input data range and put their indices into two lists. One list is 
+    for values higher than the splitting values, while another list is for values that are lower than them. Then, we return
+    a new tree node initialized with references to the left and right nodes, which are initialized with recursive calls to
+    the MakeIsolationTree() method.
+    */
 
     template <size_t Cols>
     class IsolationForest {
