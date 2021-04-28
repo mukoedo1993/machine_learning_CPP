2ed20310e119a6124a36bc3aa65b371f8c8dfad5
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2021-04-28 15:25:50 -0700

    Update dlib_cluster.cpp

--- Chapter4/dlib/dlib_cluster.cpp
+++ Chapter4/dlib/dlib_cluster.cpp
@@ -94,6 +94,30 @@ void DoGraphClustering(const I& inputs, const std::string& name){
   }
   std::vector<unsigned long> clusters;
   const auto num_clusters = chinese_whispers(edges, clusters);
+        /*!
+        requires
+            - is_ordered_by_index(edges) == true
+        ensures
+            - This function implements the graph clustering algorithm described in the
+              paper: Chinese Whispers - an Efficient Graph Clustering Algorithm and its
+              Application to Natural Language Processing Problems by Chris Biemann.
+            - Interprets edges as a directed graph.  That is, it contains the edges on the
+              said graph and the ordered_sample_pair::distance() values define the edge
+              weights (larger values indicating a stronger edge connection between the
+              nodes).  If an edge has a distance() value of infinity then it is considered
+              a "must link" edge.
+            - returns the number of clusters found.
+            - #labels.size() == max_index_plus_one(edges)
+            - for all valid i:
+                - #labels[i] == the cluster ID of the node with index i in the graph.  
+                - 0 <= #labels[i] < the number of clusters found
+                  (i.e. cluster IDs are assigned contiguously and start at 0) 
+            - Duplicate edges are interpreted as if there had been just one edge with a
+              distance value equal to the sum of all the duplicate edge's distance values.
+            - The algorithm performs exactly num_iterations passes over the graph before
+              terminating.
+    !*/
+
   std::cout << "Num clusters detected: " << num_clusters << std::endl;
   Clusters plot_clusters;
   for (long i = 0; i != inputs.nr(); i++){
