a603af2bb83e5b919281483112fa2a65765d188f
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-05-03 20:39:44 -0700

    Update some files.

---
+++ real_shogun_test/build/CMakeFiles/tst1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tst1.dir/shogun_test.cc.o"
+  "tst1"
+  "tst1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tst1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
