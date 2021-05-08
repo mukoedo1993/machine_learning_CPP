f008cf7bfa0c35c72786d35eb5d56de3306135a2
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-05-08 09:35:09 -0700

    Update some files.

---
+++ Chapter6/sharkML/README.md
@@ -0,0 +1,8 @@
+Here, we use sharkml to implement lda and pca methods to reduce the dimension 
+of dataset.
+
+
+```
+mkdir build && cd build
+cmake ..; cmake --build . --config Release && ./tst1 ../../data
+```
--- Chapter6/sharkML/tst1.cpp
+++ Chapter6/sharkML/tst1.cpp
@@ -1,5 +1,5 @@
 
-
+//https://www.shark-ml.org/sphinx_pages/build/html/rest_sources/tutorials/algorithms/pca.html?highlight=pca
 
 #include <plotcpp_clone/plotcpp/plot.h>
 
@@ -10,14 +10,15 @@
 #include <shark/Data/Dataset.h>
 #include <shark/Models/Kernels/GaussianRbfKernel.h>
 
+
 #include <experimental/filesystem>
 #include <iostream>
 #include <unordered_map>
 
-
 namespace fs = std::experimental::filesystem;
 
 using namespace shark;
+
 
 const std::vector<std::string> colors{"black", "red", "blue", "green", "cyan"};
 
@@ -44,13 +45,13 @@ void PlotClusters(const Clusters& clusters,
   plt.GnuplotCommand("set grid");
 
   auto draw_state = plt.StartDraw2D<Coords::const_iterator>();
-  for (auto& cluster : clusters) {
+  for (const auto& cluster : clusters) {
     std::stringstream params;
     params << "lc rgb '" << colors[cluster.first] << "' pt 7";
     plt.AddDrawing(draw_state,
                    plotcpp::Points(
-                       cluster.second.first.begin(), cluster.second.first.end(),
-                       cluster.second.second.begin(),
+                       cluster.second.first.cbegin(), cluster.second.first.cend(),
+                       cluster.second.second.cbegin(),
                        std::to_string(cluster.first) + " cls", params.str()));
   }
 
@@ -58,27 +59,32 @@ void PlotClusters(const Clusters& clusters,
   plt.Flush();
 }
 
+
 void PCAReduction(const UnlabeledData<RealVector>& data,
-                  const UnlabeledData<RealVector>& lables,
-                  size_t target_dim) {
+                  const UnlabeledData<RealVector>& labels,
+                  size_t target_dim)
+{
   PCA pca(data);
   LinearModel<> encoder;
   pca.encoder(encoder, target_dim);
 
+
   auto new_data = encoder(data);
+
 
   Clusters clusters;
 
   for (size_t i = 0; i < new_data.numberOfElements(); ++i) {
     auto x = new_data.element(i)[0];
     auto y = new_data.element(i)[1];
-    auto label = static_cast<int>(lables.element(i)[0]);
+    auto label = static_cast<int>(labels.element(i)[0]);
     clusters[label].first.push_back(x);
     clusters[label].second.push_back(y);
   }
 
-  PlotClusters(clusters, "PCA", "pca-sharkml.png");
+  PlotClusters(clusters, "PCA", "../results/pca-sharkml.png");
 }
+
 
 void LDAReduction(const UnlabeledData<RealVector>& data,
                   const UnlabeledData<RealVector>& labels,
@@ -86,58 +92,61 @@ void LDAReduction(const UnlabeledData<RealVector>& data,
   LinearClassifier<> encoder;
   LDA lda;
 
+
   LabeledData<RealVector, unsigned int> dataset(
-      labels.numberOfElements(), InputLabelPair<RealVector, unsigned int>(
-                                     RealVector(data.element(0).size()), 0));
+    labels.numberOfElements(), InputLabelPair<RealVector, unsigned int>(
+                                    RealVector(data.element(0).size()), 0));
 
-  for (size_t i = 0; i < labels.numberOfElements(); ++i) {
-    // labels should start from 0
-    dataset.element(i).label =
-        static_cast<unsigned int>(labels.element(i)[0]) - 1;
-    dataset.element(i).input = data.element(i);
-  }
-  lda.train(encoder, dataset);
-
-  // project data
-  auto new_labels = encoder(data);
-  auto dc = encoder.decisionFunction();
-  auto new_data = dc(data);
-
-  Clusters clusters;
-  for (size_t i = 0; i < new_data.numberOfElements(); ++i) {
-    auto l = new_labels.element(i);
-    auto x = new_data.element(i)[l];
-    auto y = new_data.element(i)[l];
-    auto label = static_cast<int>(labels.element(i)[0]);
-    clusters[label].first.push_back(x);
-    clusters[label].second.push_back(y);
-  }
-
-  PlotClusters(clusters, "LDA", "lda-sharkml.png");
-}
-
-int main(int argc, char** argv) {
-  if (argc > 1) {
-    auto data_dir = fs::path(argv[1]);
-    auto data_file_path = data_dir / data_file_name;
-    auto labels_file_path = data_dir / labels_file_name;
-    if (fs::exists(data_file_path) && fs::exists(labels_file_path)) {
-      UnlabeledData<RealVector> data;
-      importCSV(data, data_file_path, ' ');
-
-      UnlabeledData<RealVector> labels;
-      importCSV(labels, labels_file_path);
-
-      int target_dim = 2;
-      PCAReduction(data, labels, target_dim);
-      LDAReduction(data, labels, target_dim);
-
-    } else {
-      std::cerr << "Dataset file " << data_file_path << " missed\n";
+    for (size_t i = 0; i < labels.numberOfElements(); ++i) {
+      // labels should start from 0
+      dataset.element(i).label = 
+         static_cast<unsigned int>(labels.element(i)[0]) - 1;
+      dataset.element(i).input = data.element(i);
     }
-  } else {
-    std::cerr << "Please provider path to the datasets folder\n";
-  }
+    lda.train(encoder, dataset);
 
-  return 0;
+    // project data
+    auto new_labels = encoder(data);
+    auto dc = encoder.decisionFunction();
+    auto new_data = dc(data);
+
+    Clusters clusters;
+    for (size_t i = 0; i < new_data.numberOfElements(); ++i) {
+      auto l = new_labels.element(i);
+      auto x = new_data.element(i)[l];
+      auto y = new_data.element(i)[l];
+      auto label = static_cast<int>(labels.element(i)[0]);
+      clusters[label].first.push_back(x);
+      clusters[label].second.push_back(y);
+    }
+
+
+    PlotClusters(clusters, "LDA", "../results/lda-sharkml.png");
 }
+
+
+        int main(int argc, char** argv) {
+          if (argc > 1) {
+            auto data_dir = fs::path(argv[1]);
+            auto data_file_path = data_dir / data_file_name;
+            auto labels_file_path = data_dir / labels_file_name;
+          
+          if (fs::exists(data_file_path) && fs::exists(labels_file_path)) {
+            UnlabeledData<RealVector> data;
+            importCSV(data, data_file_path, ' ');
+
+            UnlabeledData<RealVector> labels;
+            importCSV(labels, labels_file_path);
+
+            int target_dim = 2;
+            PCAReduction(data, labels, target_dim);
+            LDAReduction(data, labels, target_dim);
+
+
+          } else {
+            std::cerr << "Dataset file " << data_file_path << " missed\n";
+          }} else {
+            std::cerr << "Please provide path to the dataset directory\n";
+          }
+        }
+
