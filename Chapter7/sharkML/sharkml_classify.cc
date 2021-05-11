479bae0ccd7e29980370880c5797c58168d75857
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-05-11 01:58:12 -0700

    new file:   sharkml_classify.cc

---
+++ Chapter7/sharkML/sharkml_classify.cc
@@ -0,0 +1,10 @@
+
+#include <plotcpp_clone/plotcpp/plot.h>
+
+
+#define SHARK_CV_VERBOSE_1
+#include <shark/Algorithms/KMeans.h>
+#include <shark/Algorithms/NearestNeighbors/TreeNearestNeighbors.h>
+#include <shark/Algorithms/Trainers/CSvmTrainer.h>
+#include <shark/Algorithms/Trainers/LogisticRegression.h>
+//line 8
